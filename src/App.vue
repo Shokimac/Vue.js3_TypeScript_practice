e7fe7f20bae6d839019db582143bcff89bfcb13b
Shokimac <64714255+Shokimac@users.noreply.github.com>
2022-06-11 12:39:47 +0900

    コンポーネントに文字を追加してみる

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import Payment from './components/Payment.vue';
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello World" />
+  <Payment />
 </template>
 
 <style>
